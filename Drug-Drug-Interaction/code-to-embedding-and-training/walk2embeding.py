6f96999056998f5f3c1706a86eba4f66837dae9f
LiangYu-Xidian <lyu@xidian.edu.cn>
2020-05-09 19:29:03 +0800

    Update walk2embeding.py

--- Drug-Drug-Interaction/code-to-embedding-and-training/walk2embeding.py
+++ Drug-Drug-Interaction/code-to-embedding-and-training/walk2embeding.py
@@ -14,7 +14,7 @@ def load_walks():
 	return walks
 
 walks=load_walks()
-#####size 5维 window 5 size 
+#####size 5 dimension window 5 size 
 #model = Word2Vec(walks, size=50, window=4, min_count=10, sg=1, workers=8, iter=20, hs=0, negative=100)
 model = Word2Vec(walks, size=60, window=4, sg=1, min_count=100 ,workers=10, iter=40, hs=0, negative=6)
 #hs=0,negative=6
